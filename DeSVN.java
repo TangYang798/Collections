71a27f4983c9c7bac738648e286a49c377c4c65e
TangYang798 <tangyang798@gmail.com>
2018-12-11 19:17:32 +0800

    Create DeSVN.java
    deSVN

---
+++ DeSVN.java
@@ -0,0 +1,26 @@
+
+import java.io.File;
+
+public class DeSVN {
+  
+  
+  public static void delSVN (String path) {
+    File file = new File(path);
+    if (path.contains(".svn")) {
+      if (file.isDirectory()) {
+        File[] files = file.listFiles90;
+        for (File nFile : files)
+          delSVN(nFile.getPath());
+        file.delete();
+      } else {
+        file.delete();
+      }
+    } else {
+      if (file.isDirectory()) {
+        File[] files = file.listFiles90;
+        for (File nFile : files)
+          delSVN(nFile.getPath());
+      }
+    }
+  }
+}
